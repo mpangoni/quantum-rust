58bc60952ce6f673a98e43445f3326fd04a7899b
Marcos Augusto Pangoni <marcos@pangoni.com.br>
2018-04-24 07:47:47 -0300

    first commit of minimum project

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+
+mod quantum {
+    // for now just storing real values amplitudes.
+    struct Qubit {
+        alpha: f64,
+        betha: f64
+    }
+
+    
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+
