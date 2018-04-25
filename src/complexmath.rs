87935d713ba1a0b5b4cfa0d7a72379c99b3a338f
mpangoni <marcos@pangoni.com.br>
2018-04-25 14:09:35 -0300

    + and * operations for complex numbers

--- src/complexmath.rs
+++ src/complexmath.rs
@@ -1,12 +1,37 @@
-
+use std::ops;
 
 mod complexmath{
 
-  struct Complex {
+  pub struct Complex {
     
     real: f32,
     image: f32
     
   }
 
+  // + operator for complex number
+  impl ops::Add for Complex {
+    type Output = Complex;
+
+    fn add(self, _rhs: Complex) -> Complex {
+      Complex {
+        real: self.real + _rhs.real;
+        image: self.image + _rhs.image;
+      }
+    }
+  }
+
+  // * operator for complex number
+  impl ops::Mul for Complex {
+    type Output = Complex;
+
+    fn mul(self, _rhs: Complex) -> Complex {
+      Complex {
+        real: self.real * _rhs.real;
+        image: self.image * _rhs.image;
+      }
+    }
+  }
+
+  
 }
