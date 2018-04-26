e84383cbc8a960a24b5a203cf8cf741b5eed9ef1
Marcos Augusto Pangoni <marcos@pangoni.com.br>
2018-04-25 22:26:29 -0300

    some small tests about complex numbers

--- src/complexmath.rs
+++ src/complexmath.rs
@@ -1,37 +1,63 @@
 use std::ops;
 
-mod complexmath{
-
-  pub struct Complex {
-    
-    real: f32,
-    image: f32
-    
-  }
-
-  // + operator for complex number
-  impl ops::Add for Complex {
-    type Output = Complex;
-
-    fn add(self, _rhs: Complex) -> Complex {
-      Complex {
-        real: self.real + _rhs.real;
-        image: self.image + _rhs.image;
-      }
-    }
-  }
-
-  // * operator for complex number
-  impl ops::Mul for Complex {
-    type Output = Complex;
-
-    fn mul(self, _rhs: Complex) -> Complex {
-      Complex {
-        real: self.real * _rhs.real;
-        image: self.image * _rhs.image;
-      }
-    }
-  }
-
+pub struct Complex {
   
+  pub real: f32,
+  pub image: f32
+  
+}
+
+// + operator for complex number
+impl ops::Add for Complex {
+  type Output = Complex;
+
+  fn add(self, _rhs: Complex) -> Complex {
+    Complex {
+      real: self.real + _rhs.real,
+      image: self.image + _rhs.image
+    }
+  }
+}
+
+// * operator for complex number
+impl ops::Mul for Complex {
+  type Output = Complex;
+
+  fn mul(self, _rhs: Complex) -> Complex {
+    Complex {
+      real: self.real * _rhs.real,
+      image: self.image * _rhs.image
+    }
+  }
+} 
+
+
+#[cfg(test)]
+mod tests {
+
+  use super::*;
+
+  #[test]
+  fn test_addition() {
+
+    let a = Complex { real: 1.0, image: 0.0};
+    let b = Complex { real: 0.5, image: 0.34};
+
+    let result =  a + b;
+    assert_eq!( result.real, 1.5);
+    assert_eq!( result.image, 0.34);
+  }
+
+  #[test]
+  fn test_multiplication() {
+
+    let a = Complex { real: 2.0, image: 4.0};
+    let b = Complex { real: 0.5, image: 0.5};
+
+    let result =  a * b;
+    assert_eq!( result.real, 1.0);
+    assert_eq!( result.image, 2.0);
+    
+  }
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,44 @@
 
 mod complexmath;
-use complexmath::Complex as Complex;
 
 mod quantum {
 
+    use complexmath::Complex;
+    
     // in future use complex numbers structure.
-    struct Qubit {
+    pub struct Qubit {
         
         alpha: Complex,
         betha: Complex
     }
 
-    const ZERO : Qubit = Qubit { 
+    pub const ZERO : Qubit = Qubit { 
         alpha: Complex {real: 1.0, image:0.0 },
         betha: Complex {real: 0.0, image:0.0 } };
     
-    const ZERO : Qubit = Qubit { 
+    pub const ONE : Qubit = Qubit { 
         alpha: Complex {real: 0.0, image:0.0 },
         betha: Complex {real: 1.0, image:0.0 } };
 
     // unitary gates
-    fn h(q: Qubit) { // Hadamard
+    pub fn h(q: Qubit) { // Hadamard
         
     }
 
-    fn x(q: Qubit) { //   Pauli-X gate or NOT gate
+    pub fn x(q: Qubit) { //   Pauli-X gate or NOT gate
 
     }
     
     // controled gates
-    fn cnot(c: Qubit, t: Qubit) {
+    pub fn cnot(c: Qubit, t: Qubit) {
     
     }
     
-    fn ccnot(c1: Qubit, c2: Qubit, t:Qubit) { // Toffoli Gate
+    pub fn ccnot(c1: Qubit, c2: Qubit, t:Qubit) { // Toffoli Gate
         
     }
     
-    fn cswap(c: Qubit, t1: Quibit, t2:Qubit) { // Fredkin Gate
+    pub fn cswap(c: Qubit, t1: Qubit, t2:Qubit) { // Fredkin Gate
     
     }
 
@@ -45,10 +46,7 @@ mod quantum {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
 }
 
 
